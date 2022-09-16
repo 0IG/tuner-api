67947085ab744c3b177e97f0f9823181f10a0090
Isaac Gonzalez <u1337x@gmail.com>
2022-09-16 13:04:45 -0400

    save

---
+++ db/seed.sql
@@ -0,0 +1,23 @@
+\c song_db;
+insert into songs (name, artist, album, time, is_favorite)
+values (
+        'One Dance',
+        'Drake',
+        'Views',
+        '2:53',
+        TRUE
+    ),
+    (
+        '505',
+        'Arctic Monkeys',
+        'Favourite Worst Nightmare',
+        '4:13',
+        TRUE
+    ),
+    (
+        'Talk',
+        'Yeat',
+        'Talk',
+        '2:54',
+        TRUE
+    );
---
+++ validations/checkSongs.js
@@ -0,0 +1,70 @@
+const { reset } = require("nodemon");
+
+const checkName = (req, res, next) => {
+  if (req.body.name) {
+    next();
+  } else {
+    res.status(400).json({ error: "Name is required" });
+  }
+};
+
+const checkArtist = (req, res, next) => {
+  if (req.body.artist) {
+    next();
+  } else {
+    res.status(400).json({ error: "Artist is required" });
+  }
+};
+
+const checkAlbum = (req, res, next) => {
+  if (req.body.album) {
+    next();
+  } else {
+    res.status(400).json({ error: "Album is required" });
+  }
+};
+
+const checkTime = (req, res, next) => {
+  if (req.body.time) {
+    next();
+  } else {
+    res.status(400).json({ error: "Time is required" });
+  }
+};
+
+const checkBoolean = (req, res, next) => {
+  const { is_favorite } = req.body;
+  if (
+    is_favorite == "true" ||
+    is_favorite == "false" ||
+    is_favorite == undefined
+  ) {
+    next();
+  } else {
+    res.status(400).json({ error: "is_favorite must be a boolean value" });
+  }
+};
+
+const checkForNoAdditionalParams = (req, res, next) => {
+  const { name, artist, album, time, is_favorite, ...otherStuff } = req.body;
+  if (
+    otherStuff &&
+    Object.keys(otherStuff).length === 0 &&
+    Object.getPrototypeOf(otherStuff) === Object.prototype
+  ) {
+    next();
+  } else {
+    res
+      .status(400)
+      .send({ error: "keep it short and sweet: no extra paramaters!" });
+  }
+};
+
+module.exports = {
+  checkName,
+  checkBoolean,
+  checkArtist,
+  checkAlbum,
+  checkTime,
+  checkForNoAdditionalParams,
+};
